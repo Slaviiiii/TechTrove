d946e31bc18162413ef708f268541a39dfb8f962
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-07-07 11:55:15 +0300

    update

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation/navigation.component';
-
-
+import { GlobalLoaderComponent } from './global-loader/global-loader.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
-    NavigationComponent
+    NavigationComponent,
+    GlobalLoaderComponent
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    SharedModule
+  ],
+  exports: [GlobalLoaderComponent]
 })
 export class CoreModule { }
