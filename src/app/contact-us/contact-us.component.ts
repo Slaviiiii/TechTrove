15ebd02364f0ab7b7be41e9444e9ea3ef6114dea
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-07-13 18:02:36 +0300

    update

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,31 +1,8 @@
 import { Component } from '@angular/core';
-import * as nodemailer from 'nodemailer';
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.css']
 })
-export class ContactUsComponent {
-  async sendEmail() {
-    try {
-      const host = process.env['EMAIL_HOST'] || '';
-      const port = parseInt(process.env['EMAIL_PORT'] || '0'); 
-      const secure = process.env['EMAIL_SECURE'] === 'true'; 
-      const user = process.env['EMAIL_USER'] || '';
-      const pass = process.env['EMAIL_PASSWORD'] || '';
-
-      const transporter = nodemailer.createTransport({
-        host,
-        port,
-        secure,
-        auth: {
-          user,
-          pass
-        }
-      });
-    } catch (error) {
-      console.error('Error sending email:', error);
-    }
-  }
-}
+export class ContactUsComponent {}
