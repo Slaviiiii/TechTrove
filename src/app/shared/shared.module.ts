6b18340737003a69bd41ed90af3db31162912ec8
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-07-14 14:49:49 +0300

    basic update

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpinnerComponent } from './spinner/spinner.component';
-import { CompletedPopupComponent } from './completed-popup/completed-popup.component'
+import { CompletedPopupComponent } from './completed-popup/completed-popup.component';
+import { NotCompletedPopupComponent } from './not-completed-popup/not-completed-popup.component'
 
 
 
 @NgModule({
   declarations: [
     SpinnerComponent,
-    CompletedPopupComponent
+    CompletedPopupComponent,
+    NotCompletedPopupComponent
   ],
   imports: [
     CommonModule
   ],
-  exports: [SpinnerComponent, CompletedPopupComponent]
+  exports: [SpinnerComponent, CompletedPopupComponent, NotCompletedPopupComponent]
 })
 export class SharedModule {}
