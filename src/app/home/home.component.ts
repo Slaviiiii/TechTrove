afb8f3c10c50d1ee6324e6f357e2e55ce7bc1c40
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-07-17 14:52:29 +0300

    updated home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,29 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { FirebaseService } from '../firebase.service';
-import { Product } from '../types/product';
+import { Component, OnInit } from "@angular/core";
+import { FirebaseService } from "../firebase.service";
+import { Product } from "../types/product";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
   FirebaseService: any;
   promotions: Product[] = [];
   isLoading: boolean = true;
-  
-  constructor (public firebaseService: FirebaseService) {}
+
+  constructor(public firebaseService: FirebaseService) {}
 
   ngOnInit(): void {
     this.firebaseService.getProducts().subscribe({
       next: (products: Product[]) => {
-        this.promotions = this.firebaseService.getArrayValues(products).filter(x => x.promotion).slice(0, 2);
+        this.promotions = this.firebaseService
+          .getArrayValues(products)
+          .filter((x) => x.promotion)
+          .slice(0, products.length);
         this.isLoading = false;
-    },
+      },
       error: (err: any) => {
         this.isLoading = false;
-        console.error('Error:', err);
-      }
+        console.error("Error:", err);
+      },
     });
   }
 }
